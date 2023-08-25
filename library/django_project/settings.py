1f0c864d5d7f4bb0e03e10c14f968e32ba618e56
Elvin M. Guluzada <elvin.guluzada@prodata.az>
2023-08-25 13:16:01 +0400

    New updates for Heroku deployment

--- library/django_project/settings.py
+++ library/django_project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-b4_71_jw_9z8r)t@d40wc=+w*u(ihe*(wskjl&4c(ujl!4ryu=
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [".herokuapp.com", "localhost", "127.0.0.1"]
 
 
 # Application definition
